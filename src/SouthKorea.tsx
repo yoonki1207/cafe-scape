9159caf8229b300f9ff6ccc043f8bb18c3f7cfba
yoonki1207 <yoonki1207@github.com>
2022-08-09 18:23:30 +0900

    init commit south Korea map create

---
+++ src/SouthKorea.tsx
@@ -0,0 +1,18 @@
+import southkorea from "./southKoreaSvg";
+// import "./App.css";
+
+const SouthKorea = () => {
+  return (
+    <svg
+      width="100%"
+      height="100vh"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+    >
+      {southkorea}
+    </svg>
+  );
+};
+
+export default SouthKorea;
